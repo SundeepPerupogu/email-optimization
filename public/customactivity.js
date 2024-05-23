3c5496b1ceb92d3407e6c68f119ff78215d4dd95
SundeepPerupogu <95463949+SundeepPerupogu@users.noreply.github.com>
2024-05-23 17:51:51 +0530

    Update customactivity.js

--- public/customactivity.js
+++ public/customactivity.js
@@ -1,90 +1,138 @@
-define([
-    'postmonger'
-], function (Postmonger) {
+define( function( require ) {
+
     'use strict';
+    
+	var Postmonger = require( 'postmonger' );
+	var $ = require( 'vendor/jquery.min' );
 
     var connection = new Postmonger.Session();
-    var payload = {};
-    var steps = [
-        { label: 'Configure Activity', key: 'step1' }
-    ];
-    var currentStep = steps[0].key;
-
+    var toJbPayload = {};
+    var step = 1; 
+	var tokens;
+	var endpoints;
+	
     $(window).ready(onRender);
 
-    connection.on('initActivity', initialize);
-    connection.on('requestedTokens', onGetTokens);
-    connection.on('requestedEndpoints', onGetEndpoints);
-    connection.on('clickedNext', onClickedNext);
-    connection.on('clickedBack', onClickedBack);
-    connection.on('gotoStep', onGotoStep);
+    connection.on('initActivity', function(payload) {
+        var priority;
+
+        if (payload) {
+            toJbPayload = payload;
+            console.log('payload',payload);
+            
+			//merge the array of objects.
+			var aArgs = toJbPayload['arguments'].execute.inArguments;
+			var oArgs = {};
+			for (var i=0; i<aArgs.length; i++) {  
+				for (var key in aArgs[i]) { 
+					oArgs[key] = aArgs[i][key]; 
+				}
+			}
+			//oArgs.priority will contain a value if this activity has already been configured:
+			priority = oArgs.priority || toJbPayload['configurationArguments'].defaults.priority;            
+        }
+        
+		$.get( "/version", function( data ) {
+			$('#version').html('Version: ' + data.version);
+		});                
+
+        // If there is no priority selected, disable the next button
+        if (!priority) {
+            connection.trigger('updateButton', { button: 'next', enabled: false });
+        }
+
+		$('#selectPriority').find('option[value='+ priority +']').attr('selected', 'selected');		
+		gotoStep(step);
+        
+    });
+
+    connection.on('requestedTokens', function(data) {
+		if( data.error ) {
+			console.error( data.error );
+		} else {
+			tokens = data;
+		}        
+    });
+
+    connection.on('requestedEndpoints', function(data) {
+		if( data.error ) {
+			console.error( data.error );
+		} else {
+			endpoints = data;
+		}        
+    });
+
+    connection.on('clickedNext', function() {
+        step++;
+        gotoStep(step);
+        connection.trigger('ready');
+    });
+
+    connection.on('clickedBack', function() {
+        step--;
+        gotoStep(step);
+        connection.trigger('ready');
+    });
 
     function onRender() {
         connection.trigger('ready');
+
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
-    }
 
-    function initialize(data) {
-        if (data) {
-            payload = data;
-        }
-
-        var inArguments = payload['arguments'] && payload['arguments'].execute && payload['arguments'].execute.inArguments || [];
-
-        $.each(inArguments, function (index, inArgument) {
-            if (inArgument.futureUtcTime) {
-                $('#futureUtcTime').val(inArgument.futureUtcTime);
-            }
-
-            if (inArgument.userTimeZone) {
-                $('#userTimeZone').val(inArgument.userTimeZone);
-            }
+        // Disable the next button if a value isn't selected
+        $('#selectPriority').change(function() {
+            var priority = getPriority();
+            connection.trigger('updateButton', { button: 'next', enabled: Boolean(priority) });
         });
+    };
 
-        connection.trigger('updateButton', { button: 'next', text: 'done', visible: true });
-    }
-
-    function onGetTokens(tokens) {
-        // Handle tokens
-    }
-
-    function onGetEndpoints(endpoints) {
-        // Handle endpoints
-    }
-
-    function onClickedNext() {
-        save();
-        connection.trigger('nextStep');
-    }
-
-    function onClickedBack() {
-        connection.trigger('prevStep');
-    }
-
-    function onGotoStep(step) {
-        showStep(step);
-        connection.trigger('ready');
-    }
-
-    function showStep(step) {
-        currentStep = step;
-
+    function gotoStep(step) {
         $('.step').hide();
-        $('#' + step).show();
-    }
+        switch(step) {
+            case 1:
+                $('#step1').show();
+                connection.trigger('updateButton', { button: 'next', text: 'next', enabled: Boolean(getPriority()) });
+                connection.trigger('updateButton', { button: 'back', visible: false });
+                break;
+            case 2:
+                $('#step2').show();
+                $('#showPriority').html(getPriority());
+                connection.trigger('updateButton', { button: 'back', visible: true });
+                connection.trigger('updateButton', { button: 'next', text: 'done', visible: true });
+                break;
+            case 3: // Only 2 steps, so the equivalent of 'done' - send off the payload
+                save();
+                break;
+        }
+    };
+
+    function getPriority() {
+        return $('#selectPriority').find('option:selected').attr('value').trim();
+    };
 
     function save() {
-        var futureUtcTime = $('#futureUtcTime').val();
-        var userTimeZone = $('#userTimeZone').val();
 
-        payload['arguments'].execute.inArguments = [{
-            "futureUtcTime": futureUtcTime,
-            "userTimeZone": userTimeZone
-        }];
+        var value = getPriority();
 
-        payload['metaData'].isConfigured = true;
+        // toJbPayload is initialized on populateFields above.  Journey Builder sends an initial payload with defaults
+        // set by this activity's config.json file.  Any property may be overridden as desired.
+        //toJbPayload.name = "my activity";
 
-        connection.trigger('updateActivity', payload);
-    }
+		//this will be sent into the custom activity body within the inArguments array.
+        toJbPayload['arguments'].execute.inArguments.push({"priority": value});
+
+		/*
+        toJbPayload['metaData'].things = 'stuff';
+        toJbPayload['metaData'].icon = 'path/to/icon/set/from/iframe/icon.png';
+        toJbPayload['configurationArguments'].version = '1.1'; // optional - for 3rd party to track their customActivity.js version
+        toJbPayload['configurationArguments'].partnerActivityId = '49198498';
+        toJbPayload['configurationArguments'].myConfiguration = 'configuration coming from iframe';
+		*/
+		
+		toJbPayload.metaData.isConfigured = true;  //this is required by JB to set the activity as Configured.
+        connection.trigger('updateActivity', toJbPayload);
+    }; 
+    	 
 });
+			
