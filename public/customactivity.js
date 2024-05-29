b5adf6a1224edc2f7763a8d6716bbb49fac1d45a
SundeepPerupogu <95463949+SundeepPerupogu@users.noreply.github.com>
2024-05-29 20:01:02 +0530

    Update customactivity.js

--- public/customactivity.js
+++ public/customactivity.js
@@ -3,87 +3,43 @@ define([
 ], function (Postmonger) {
     'use strict';
 
+											  
     var connection = new Postmonger.Session();
-    var payload = {};
-    var steps = [
-        { label: 'Configure Activity', key: 'step1' }
-    ];
-    var currentStep = steps[0].key;
 
     $(window).ready(onRender);
 
     connection.on('initActivity', initialize);
-    connection.on('requestedTokens', onGetTokens);
-    connection.on('requestedEndpoints', onGetEndpoints);
-    connection.on('clickedNext', onClickedNext);
-    connection.on('clickedBack', onClickedBack);
-    connection.on('gotoStep', onGotoStep);
+    connection.on('clickedNext', save);
 
     function onRender() {
         connection.trigger('ready');
-        connection.trigger('requestTokens');
-        connection.trigger('requestEndpoints');
     }
 
-    function initialize(data) {
-        if (data) {
-            payload = data;
+    function initialize(payload) {
+        if (payload.arguments && payload.arguments.execute && payload.arguments.execute.inArguments) {
+            const inArguments = payload.arguments.execute.inArguments;
+            const startWindow = inArguments.find(arg => arg.start_window).start_window;
+            const endWindow = inArguments.find(arg => arg.end_window).end_window;
+            
+            $('#start_window').val(startWindow);
+            $('#end_window').val(endWindow);
         }
-
-        var inArguments = payload['arguments'] && payload['arguments'].execute && payload['arguments'].execute.inArguments || [];
-
-        $.each(inArguments, function (index, inArgument) {
-            if (inArgument.futureUtcTime) {
-                $('#futureUtcTime').val(inArgument.futureUtcTime);
-            }
-
-            if (inArgument.userTimeZone) {
-                $('#userTimeZone').val(inArgument.userTimeZone);
-            }
-        });
-
-        connection.trigger('updateButton', { button: 'next', text: 'done', visible: true });
-    }
-
-    function onGetTokens(tokens) {
-        // Handle tokens
-    }
-
-    function onGetEndpoints(endpoints) {
-        // Handle endpoints
-    }
-
-    function onClickedNext() {
-        save();
-        connection.trigger('nextStep');
-    }
-
-    function onClickedBack() {
-        connection.trigger('prevStep');
-    }
-
-    function onGotoStep(step) {
-        showStep(step);
-        connection.trigger('ready');
-    }
-
-    function showStep(step) {
-        currentStep = step;
-
-        $('.step').hide();
-        $('#' + step).show();
     }
 
     function save() {
-        var futureUtcTime = $('#futureUtcTime').val();
-        var userTimeZone = $('#userTimeZone').val();
-
-        payload['arguments'].execute.inArguments = [{
-            "futureUtcTime": futureUtcTime,
-            "userTimeZone": userTimeZone
-        }];
-
-        payload['metaData'].isConfigured = true;
+        const startWindow = $('#start_window').val();
+        const endWindow = $('#end_window').val();
+        
+        const payload = {
+            arguments: {
+                execute: {
+                    inArguments: [
+                        { start_window: startWindow },
+                        { end_window: endWindow }
+                    ]
+                }
+            }
+        };
 
         connection.trigger('updateActivity', payload);
     }
