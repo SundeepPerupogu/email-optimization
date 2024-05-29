bade8640e0b7ba5cd954a295ff5b03c89d702232
SundeepPerupogu <95463949+SundeepPerupogu@users.noreply.github.com>
2024-05-29 17:54:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const SendTimeCalculator = require('./customActivity');
+const SendTimeCalculator = require('./public/customactivity');
 
 const app = express();
 app.use(bodyParser.json());
