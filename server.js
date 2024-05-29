6e8d5f9514aef7ce0e312e9e2f2bef9f79127992
SundeepPerupogu <95463949+SundeepPerupogu@users.noreply.github.com>
2024-05-29 16:12:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,12 +11,17 @@ app.post('/execute', (req, res) => {
         const calculator = new SendTimeCalculator(start_window, end_window);
         calculator.validate();
         const nextSend = calculator.calculateNextSendTime(time_zone);
+        console.log(JSON.stringify(res.body));
         res.status(200).send({ next_send: nextSend });
     } catch (error) {
         res.status(400).send({ error: error.message });
     }
 });
+app.post('/validate', (req, res) => {
+    console.log(`Validated`);
+        res.sendStatus(200);
 
+});
 app.get('/config', (req, res) => {
     res.sendFile(__dirname + '/public/config.html');
 });
