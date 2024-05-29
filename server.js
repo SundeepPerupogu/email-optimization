7286724e7c747d652e55012d43f14aabbec0433f
SundeepPerupogu <95463949+SundeepPerupogu@users.noreply.github.com>
2024-05-29 16:06:31 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const SendTimeCalculator = require('./customActivity');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/execute', (req, res) => {
+    const { time_zone, start_window, end_window } = req.body.inArguments[0];
+    try {
+        const calculator = new SendTimeCalculator(start_window, end_window);
+        calculator.validate();
+        const nextSend = calculator.calculateNextSendTime(time_zone);
+        res.status(200).send({ next_send: nextSend });
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+});
+
+app.get('/config', (req, res) => {
+    res.sendFile(__dirname + '/public/config.html');
+});
+
+app.listen(3000, () => {
+    console.log('Custom Activity Service is running on port 3000');
+});
