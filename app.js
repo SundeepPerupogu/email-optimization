edf9362301308db2983f156d6e60efce99375224
SundeepPerupogu <95463949+SundeepPerupogu@users.noreply.github.com>
2024-09-23 20:19:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -152,9 +152,10 @@ app.post('/execute', (req, res) => {
         }
 	console.log("Data type of nextSendTime: ", typeof nextSendTime);
 	console.log("Data type of nextSendTimeDateType: ", typeof nextSendTimeDateType);
-	console.log('{"result":{ "nextSendTime" : ',nextSendTime,',"nextSendTimeDateType":','nextSendTime }}');
-	return res.status(200).json({"result":{ "nextSendTime" : nextSendTime,"nextSendTimeDateType":nextSendTime }});    
+	console.log('{"result":{ "nextSendTime" : ',nextSendTime,',"nextSendTimeDateType":',nextSendTime,' }}');
+	return res.status(200).json({"result":{ "nextSendTime" : nextSendTime }});    
 	//return res.status(200).send(JSON.stringify({
+	//return res.status(200).json({"result":{ "nextSendTime" : nextSendTime,"nextSendTimeDateType":nextSendTime }});    
     	 //  nextSendTime: nextSendTime,  // Send as an ISO string for consistency
     	  // nextSendTimeDateType: nextSendTimeDateType.toISOString()
 	//}));    
